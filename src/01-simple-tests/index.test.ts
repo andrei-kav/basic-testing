c38ac69d5117b46f91bd7ff7c7ce22e269c3dfe7
kryvulia <andrei.kryvulia@datylon.svaps.com>
2024-02-10 20:14:30 +0300

    01, 02, 03, tests added

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,49 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import {simpleCalculator, Action} from './index';
 
-describe('simpleCalculator tests', () => {
+describe.only('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const input = {a: 3, b: 6, action: Action.Add}
+    expect(simpleCalculator(input)).toBe(9)
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const input = {a: 6, b: 3, action: Action.Subtract}
+    expect(simpleCalculator(input)).toBe(3)
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const input = {a: 6, b: 3, action: Action.Multiply}
+    expect(simpleCalculator(input)).toBe(18)
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const input = {a: 6, b: 3, action: Action.Divide}
+    expect(simpleCalculator(input)).toBe(2)
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const input = {a: 2, b: 3, action: Action.Exponentiate}
+    expect(simpleCalculator(input)).toBe(8)
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    [
+      {a: 2, b: 3, action: 23},
+      {a: 2, b: 3, action: 'invalidAction'},
+      {a: 2, b: 3, action: ['invalidAction']}
+    ].forEach((input: any) => {
+      expect(simpleCalculator(input)).toBe(null)
+    })
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    [
+      {a: '2', b: 3, action: Action.Add},
+      {a: 2, b: '3', action: Action.Add},
+      {a: 2, b: ['3'], action: Action.Add}
+    ].forEach((input: any) => {
+      expect(simpleCalculator(input)).toBe(null)
+    })
   });
 });
